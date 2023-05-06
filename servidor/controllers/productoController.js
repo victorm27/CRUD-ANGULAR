68d8c795328fad74072867d41ffd085dd362cfaf
victorm27 <victormanuel272791@gmail.com>
2023-05-06 17:24:20 -0500

    commit 1

---
+++ servidor/controllers/productoController.js
@@ -0,0 +1,103 @@
+const Producto = require("../models/Producto");
+
+
+exports.crearProducto = async (req, res) => {
+
+    try {
+        let producto;
+
+        // Creamos nuestro producto
+        producto = new Producto(req.body);
+
+        await producto.save();
+        res.send(producto);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+exports.obtenerProductos = async (req,res) =>{
+
+    try{
+
+        const productos = await Producto.find();
+        res.json(productos)
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+
+
+}
+exports.actualizarProducto = async (req,res) =>{
+
+    try{
+
+        const {nombre,categoria,ubicacion,precio} =req.body;
+        let producto = await Producto.findById(req.params.id);
+        
+        if(!producto){
+            res.status(404).json({  msg:'No se ha encontrado este producto'});
+        }
+        else{
+            producto.nombre = nombre;
+            producto.categoria = categoria;
+            producto.ubicacion = ubicacion;
+            producto.precio = precio;
+
+            producto = await Producto.findOneAndUpdate({_id:req.params.id},producto,{new:true});
+            res.json(producto);
+        }
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+exports.obtenerProducto = async (req,res) =>{
+
+    try{
+
+        let producto = await Producto.findById(req.params.id);
+
+        if(!producto){
+            res.status(404).json({  msg:'No se ha encontrado este producto'});
+        }
+        else{
+            res.json(producto);
+        }
+
+
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+exports.eliminarProducto = async (req,res) =>{
+
+    try{
+
+        let producto = await Producto.findById(req.params.id);
+
+        if(!producto){
+            res.status(404).json({  msg:'No se ha encontrado este producto'});
+        }
+        else{
+            producto = await Producto.findByIdAndRemove({_id:req.params.id});
+            res.json('El producto ha sido eliminado con exito');
+        }
+
+
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
---
+++ servidor/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require('cors');
+//Creamos el servidor
+const app = express();
+
+//Conectamos la base de datos
+conectarDB();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/productos', require('./routes/producto'))
+
+
+
+
+app.listen(4000, () => {
+    console.log('El servidor esta corriendo perfectamente')
+})
